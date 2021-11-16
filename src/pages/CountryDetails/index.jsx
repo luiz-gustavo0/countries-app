94a36c3cf867b1f27c51ee466d5303b5265bf143
luiz-gustavo0 <santosluizgustavo101@gmail.com>
2021-11-16 18:51:23 -0300

    adiciona pagina de detalhes do país

--- src/pages/CountryDetails/index.jsx
+++ src/pages/CountryDetails/index.jsx
@@ -1,39 +1,97 @@
-import React from 'react';
+import React, { useState } from 'react';
+import axios from 'axios';
+import { useParams, useNavigate } from 'react-router-dom';
+import { useEffect } from 'react/cjs/react.development';
+
+import * as S from './styles';
 
 export const CountryDetails = () => {
+  const { code } = useParams();
+  const [country, setCountry] = useState(null);
+
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    async function getCountry() {
+      const response = await axios.get(
+        `https://restcountries.com/v2/alpha/${code}`
+      );
+
+      setCountry(response.data);
+    }
+
+    getCountry();
+  }, [code]);
+  console.log(country);
   return (
-    <div>
-      <button>Back</button>
-      <div>
-        <div>
-          <img src='https://flagcdn.com/ag.svg' alt='' />
-        </div>
-        <div>
-          <h2>País</h2>
-          <div>
+    <S.Container>
+      <button
+        onClick={() => {
+          navigate('/', { replace: true });
+        }}
+      >
+        <span>&#8592;</span> Back
+      </button>
+      <S.Content>
+        <S.WrapperImg>
+          <img src={country?.flag} alt={`Bandeira ${country?.name}`} />
+        </S.WrapperImg>
+        <S.InfoCountry>
+          <h2>{country?.name}</h2>
+          <S.InfoCountryText>
             <div>
-              <p>Nome</p>
-              <p>População</p>
-              <p>Region</p>
-              <p>Sub Region</p>
-              <p>Capital</p>
+              <p>
+                <strong>Native Name: </strong>
+                {country?.nativeName}
+              </p>
+              <p>
+                <strong>População: </strong>
+                {country?.population}
+              </p>
+              <p>
+                <strong>Region: </strong>
+                {country?.region}
+              </p>
+              <p>
+                <strong>Sub Region: </strong>
+                {country?.subregion}
+              </p>
+              <p>
+                <strong>Capital: </strong>
+                {country?.capital}
+              </p>
             </div>
             <div>
-              <p>Top Level Domain</p>
-              <p>Currencies</p>
-              <p>Languages</p>
+              <p>
+                <strong>Top Level Domain: </strong>
+                {country?.topLevelDomain.map((domain, i) => (
+                  <span key={i}>{domain}</span>
+                ))}
+              </p>
+              <p>
+                <strong>Currencies: </strong>
+                {country?.currencies.map((currency, i) => (
+                  <span key={i}>{currency?.name}</span>
+                ))}
+              </p>
+              <p>
+                <strong>Languages: </strong>
+                {country?.languages.map((language, i) => (
+                  <span key={i}>{language?.name},</span>
+                ))}
+              </p>
             </div>
-          </div>
-          <div>
-            <span>Borders</span>
+          </S.InfoCountryText>
+          <S.InfoCountryBorders>
+            <h6>Border Countries:</h6>
             <ul>
-              <li>aa</li>
-              <li>aa</li>
-              <li>aa</li>
+              {country?.borders?.map((border, i) => (
+                <li key={i}>{border}</li>
+              ))}
             </ul>
-          </div>
-        </div>
-      </div>
-    </div>
+          </S.InfoCountryBorders>
+        </S.InfoCountry>
+      </S.Content>
+    </S.Container>
   );
 };
---
+++ src/pages/CountryDetails/styles.js
@@ -0,0 +1,79 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  padding: 6rem 0;
+
+  button {
+    margin-bottom: 6rem;
+    display: flex;
+    align-items: center;
+    gap: 0.8rem;
+    padding: 0.5rem 2rem;
+    border: 1px solid ${(props) => props.theme.elementColor};
+    background: ${(props) => props.theme.elementColor};
+    border-radius: 3px;
+    box-shadow: 1px 1px 2px rgba(0, 0, 0, 0.1);
+    font-size: 1.4rem;
+    cursor: pointer;
+  }
+`;
+
+export const Content = styled.div`
+  width: 100%;
+
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  align-items: center;
+  grid-gap: 8rem;
+`;
+
+export const WrapperImg = styled.div`
+  grid-column: 1 / 2;
+`;
+
+export const InfoCountry = styled.div`
+  grid-column: 2 / -1;
+  h2 {
+    font-size: 2.4rem;
+    margin-bottom: 3rem;
+  }
+`;
+
+export const InfoCountryText = styled.div`
+  display: flex;
+  gap: 6rem;
+  margin-bottom: 3rem;
+
+  p {
+    display: flex;
+    align-items: center;
+    gap: 1rem;
+    margin-bottom: 1rem;
+  }
+`;
+
+export const InfoCountryBorders = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 2rem;
+
+  h6 {
+    font-size: 1.6rem;
+  }
+
+  ul {
+    list-style: none;
+    display: flex;
+    align-items: center;
+    flex-wrap: wrap;
+    gap: 1rem;
+
+    li {
+      padding: 0 1rem;
+      border: 1px solid ${(props) => props.theme.elementColor};
+      background: ${(props) => props.theme.elementColor};
+      border-radius: 3px;
+      box-shadow: 1px 1px 2px rgba(0, 0, 0, 0.1);
+    }
+  }
+`;
